caea6a0d807d9fb87b23053196835a1a08081d2b
yahav123456 <166650066+yahav123456@users.noreply.github.com>
2024-05-27 00:57:53 +0300

    hh

--- backend/tests/obj/Debug/net8.0/tests.AssemblyInfo.cs
+++ backend/tests/obj/Debug/net8.0/tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+051170f4182b951fbfe5e6e1b447eed095500b22")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dab72e9828b9b9496027eea884b85190ce7f40ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
